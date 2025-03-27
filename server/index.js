7e9d8e592d47823748d71bb9c819f8df3ecf8cc0
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-03-27 22:10:10 +0300

    fix message

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const server = new ws.Server({ port: '3000'})
 
 server.on('connection', socket => {
     socket.on('message', message => {
-        console.log(message)
+        const b = Buffer.from(message)
+        console.log(b.toString())
         socket.send(`${message}`)
     })
 })
